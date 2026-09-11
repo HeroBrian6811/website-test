64b1308d05151117fc95b1a996814b22c0554caf
HeroBrian6811 <minecraft_serverhope@proton.me>
2026-09-11 17:12:07 -0500

    Add button click handler and console log

---
+++ web.js
@@ -0,0 +1,7 @@
+// Simple button click handler
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('You clicked the button!');
+});
+
+// You can add more interactivity here
+console.log('Website loaded successfully!');
